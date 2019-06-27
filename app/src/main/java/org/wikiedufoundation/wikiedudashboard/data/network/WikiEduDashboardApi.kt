9f815e2a9f13e62d7b2cccd04f83c72ab538ea45
Ujjwal Agrawal <ujjwal@codenicely.in>
2019-06-27 20:43:07 +0530

    Documentation for APIs added

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/data/network/WikiEduDashboardApi.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/data/network/WikiEduDashboardApi.kt
@@ -17,30 +17,58 @@ import retrofit2.http.Url
 
 interface WikiEduDashboardApi {
 
+    /**
+     * This API is used to fetch course details and statistics.
+     ***/
     @GET
     fun getCourseDetail(@Url url: String): Call<CourseDetailResponse>
 
+    /**
+     * This API is used to fetch list of articles of a course.
+     ***/
     @GET
     fun getArticlesEdited(@Url url: String): Call<ArticlesEdited>
 
+    /**
+     * This API is used to fetch list of articles edited in a course.
+     ***/
     @GET
     fun getRecentActivity(@Url url: String): Call<RecentActivityResponse>
 
+    /**
+     * This API is used to fetch list of students in a course.
+     ***/
     @GET
     fun getStudentList(@Url url: String): Call<StudentListResponse>
 
+    /**
+     * This API is used to fetch list of uploads by the users in a course.
+     ***/
     @GET
     fun getCourseUploads(@Url url: String): Call<CourseUploadResponse>
 
+    /**
+     * This API is used to fetch Dashboard details. Ex - My Courses - Active, Archived.
+     ***/
     @GET("dashboard.json")
     fun getDashboardDetail(@Header("Cookie") sessionIdAndToken: String): Call<MyDashboardResponse>
 
+    /**
+     * This API is used to fetch list of active courses.
+     ***/
     @GET("explore.json")
     fun getExploreCourses(@Header("Cookie") sessionIdAndToken: String): Call<ExploreCoursesResponse>
 
+    /**
+     * This API is used to fetch list of active campaigns.
+     ***/
     @GET("campaigns.json")
     fun getExploreCampaigns(@Header("Cookie") sessionIdAndToken: String): Call<ExploreCampaignsResponse>
 
+    /**
+     * This API is used to fetch media details including category list, file uses, description,
+     * license from Wikimedia Commons.
+     ***/
     @GET("w/api.php?action=query&format=json&pageids=79744855&prop=globalusage|categories|imageinfo&iiprop=size|extmetadata|url&clshow=!hidden")
     fun getMediaDetailsFromCommons(@Header("Cookie") sessionIdAndToken: String): Call<MediaDetailsResponse>
 }
