453f90e6d379d9b729c5e03d300e940ed1e4117b
gconnect <agatevureglory@gmail.com>
2019-10-05 12:42:19 +0100

    removed unused files

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/welcome/WelcomeActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import me.relex.circleindicator.CircleIndicator
 import org.wikiedufoundation.wikiedudashboard.R
 import org.wikiedufoundation.wikiedudashboard.util.ViewPagerAdapter
 import java.util.*
@@ -13,17 +15,22 @@ class WelcomeActivity : AppCompatActivity() {
     private var viewPager: ViewPager? = null
     private var viewPagerAdapter: ViewPagerAdapter? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
         viewPager = findViewById(R.id.viewPager)
         viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
         viewPager!!.adapter = viewPagerAdapter
+
+
+
+
         val fragmentList = ArrayList<Fragment>()
         val titleList = ArrayList<String>()
-        titleList.add("Home")
+        titleList.add("")
         fragmentList.add(WikiEducationDashboardFragment())
-        titleList.add("Home")
+        titleList.add("")
         fragmentList.add(OutreachProgramsDashboardFragment())
         viewPagerAdapter!!.setTabData(fragmentList, titleList)
         viewPagerAdapter!!.notifyDataSetChanged()
