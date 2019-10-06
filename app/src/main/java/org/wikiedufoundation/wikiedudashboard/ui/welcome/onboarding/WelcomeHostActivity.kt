80fa2f7a980aed69fcb37033a91b46e307698d0e
gconnect <agatevureglory@gmail.com>
2019-10-06 20:25:56 +0100

    added navigation component and indicator and safe args

---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/welcome/onboarding/WelcomeHostActivity.kt
@@ -0,0 +1,12 @@
+package org.wikiedufoundation.wikiedudashboard.ui.welcome.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeHostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_host)
+    }
+}
