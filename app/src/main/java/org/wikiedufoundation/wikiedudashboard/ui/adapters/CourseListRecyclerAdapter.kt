14fb052ac917f9393891a55f7ea356bfb89f7392
Victor <victor.a.vicari@gmail.com>
2019-10-08 10:50:11 -0300

    update the course list recycler adapter to the newest single adapter. also transform the item rv explore courses into a data binding layout and transformed the course list data to a data class

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/adapters/CourseListRecyclerAdapter.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/adapters/CourseListRecyclerAdapter.kt
@@ -1,12 +1,6 @@
 package org.wikiedufoundation.wikiedudashboard.ui.adapters
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_rv_explore_courses.view.*
-import org.wikiedufoundation.wikiedudashboard.R
+import org.wikiedufoundation.wikiedudashboard.common.SingleLayoutAdapter
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.view.CourseListFragment
 import org.wikiedufoundation.wikiedudashboard.ui.dashboard.data.CourseListData
 import java.util.*
@@ -16,22 +10,13 @@ import java.util.*
  * @constructor secondary constructor to initialize [CourseListFragment] variable
  * ***/
 class CourseListRecyclerAdapter internal constructor(
-    private var courseListFragment: CourseListFragment
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        layoutId: Int,
+        private val onItemClickListener: (String) -> Unit
+) : SingleLayoutAdapter(layoutId) {
 
     private var courses: List<CourseListData> = ArrayList()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MyDashboardViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_rv_explore_courses, parent, false))
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-
-        val course: CourseListData? = courses[position]
-        val myDashboardViewHolder = holder as MyDashboardViewHolder
-        myDashboardViewHolder.tvCourseTitle.text = course?.title
-        holder.itemView.setOnClickListener { courseListFragment.openCourseDetail(courses[position].slug) }
-    }
+    override fun getObjForPosition(position: Int): Any = courses[position]
 
     /**
      * Set [CourseListData] type [courses] list
@@ -41,16 +26,10 @@ class CourseListRecyclerAdapter internal constructor(
         this.courses = courses
     }
 
-    override fun getItemCount(): Int {
-        return courses.size
+    override fun getItemCount(): Int = courses.size
+
+    fun onCouseClicked(course: CourseListData) {
+        onItemClickListener(course.slug)
     }
 
-
-    /**
-     * Use [MyDashboardViewHolder] inner class to declare [tvCourseTitle] TextView
-     * @property itemView primary constructor property to call [tv_course_title]
-     * ***/
-    inner class MyDashboardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvCourseTitle: TextView = itemView.tv_course_title
-    }
 }
