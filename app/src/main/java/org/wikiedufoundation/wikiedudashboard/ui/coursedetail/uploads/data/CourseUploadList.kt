c37380dbcd1e699cd8b1de3f93bf06499c10e10e
Shu <sakurala111@yahoo.com>
2019-10-14 01:33:18 -0700

    chore: Code Documentation for course uploads module.  (#125)

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/uploads/data/CourseUploadList.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/uploads/data/CourseUploadList.kt
@@ -2,6 +2,12 @@ package org.wikiedufoundation.wikiedudashboard.ui.coursedetail.uploads.data
 
 import java.io.Serializable
 
+/**
+ * [CourseUploadList] model class
+ * @constructor primary constructor
+ *
+ * @property uploads list of course uploads
+ * ***/
 class CourseUploadList(
     val uploads: List<CourseUpload>
 ) : Serializable
