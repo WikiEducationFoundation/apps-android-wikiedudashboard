785015813c3d345f306d88e1ba17803fdd4af374
Shu Zhang <shu@goopter.com>
2019-10-13 21:13:38 -0700

    code documenting part 11

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/uploads/presenter/CourseUploadsPresenter.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/uploads/presenter/CourseUploadsPresenter.kt
@@ -1,5 +1,13 @@
 package org.wikiedufoundation.wikiedudashboard.ui.coursedetail.uploads.presenter
 
+/**
+ * Presenter interface that defines [requestCourseUploads]
+ * ***/
 interface CourseUploadsPresenter {
+
+    /**
+     * [requestCourseUploads] handles course uploads and display in view
+     * @param url api url
+     * ***/
     fun requestCourseUploads(url: String)
 }
