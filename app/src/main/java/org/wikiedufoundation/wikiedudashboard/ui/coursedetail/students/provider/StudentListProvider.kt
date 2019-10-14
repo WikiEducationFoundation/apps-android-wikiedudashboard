fd1756b917e42dedb2410c32fb6cc39309c4769b
Shu <sakurala111@yahoo.com>
2019-10-14 01:36:41 -0700

    chore: Code Documentation for Student List module. #123

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/students/provider/StudentListProvider.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/students/provider/StudentListProvider.kt
@@ -2,6 +2,15 @@ package org.wikiedufoundation.wikiedudashboard.ui.coursedetail.students.provider
 
 import org.wikiedufoundation.wikiedudashboard.util.PresenterCallback
 
+/**
+ * Interface that defines [requestStudentList] method
+ * ***/
 interface StudentListProvider {
+
+    /**
+     * Implementing [requestStudentList] to get list of student data
+     * @param url api url
+     * @param presenterCallback PresenterCallback variable
+     * ***/
     fun requestStudentList(url: String, presenterCallback: PresenterCallback<*>)
 }
--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/students/view/StudentListFragment.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/students/view/StudentListFragment.kt
@@ -20,6 +20,9 @@ import org.wikiedufoundation.wikiedudashboard.ui.profile.ProfileActivity
 import org.wikiedufoundation.wikiedudashboard.util.showToast
 import timber.log.Timber
 
+/**
+ * A [Fragment] that displays list of students
+ * ***/
 class StudentListFragment : Fragment(), StudentListView {
 
     private var recyclerView: RecyclerView? = null
@@ -78,6 +81,11 @@ class StudentListFragment : Fragment(), StudentListView {
         }
     }
 
+    /**
+     * Use [openStudentProfile] to send [username] to [ProfileActivity]
+     *
+     * @param username user's username in String
+     * ***/
     fun openStudentProfile(username: String) {
         val i = Intent(context, ProfileActivity::class.java)
         i.putExtra("username", username)
