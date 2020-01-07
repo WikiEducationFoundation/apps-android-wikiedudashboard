8c696bb4f618ad9f6a078899ba624062d187edc4
mistervaibhav <vebatlow2@gmail.com>
2020-01-07 22:07:00 +0530

    PresenterCallback changed from java to kotlin

---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/util/PresenterCallback.kt
@@ -0,0 +1,10 @@
+package org.wikiedufoundation.wikiedudashboard.util
+
+/**
+ * Retrofit2 HTTP request result callbacks
+ */
+interface PresenterCallback<T> {
+    fun onSuccess(t: T)
+    fun onFailure()
+
+}
