3bbbbc765d3ddaadc85add48a50fb2b7ddbf07af
mistervaibhav <vebatlow2@gmail.com>
2020-01-14 22:00:02 +0530

    conflicts resolved and kDoc added

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/util/PresenterCallback.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/util/PresenterCallback.kt
@@ -2,8 +2,14 @@ package org.wikiedufoundation.wikiedudashboard.util
 
 /**
  * Retrofit2 HTTP request result callbacks
+ * @param T
  */
 interface PresenterCallback<T> {
+
+    /**
+     * @param t
+     */
     fun onSuccess(t: T)
+    
     fun onFailure()
 }
