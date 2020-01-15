541f024409bd902a7e9d0da8097eca88fcf2dee5
gconnect <agatevureglory@gmail.com>
2020-01-15 01:31:13 +0100

    Changed the single to viewModel in the viewModelModule

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -1,10 +1,11 @@
 package org.wikiedufoundation.wikiedudashboard.di
 
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
 
 val viewModelModule = module {
-    single { ActiveCampaignViewModel(get()) }
-    single { CourseListViewModel(get()) }
+    viewModel { ActiveCampaignViewModel(get()) }
+    viewModel { CourseListViewModel(get()) }
 }
