89a535596627928cace809e6c66305c09b7aca71
gconnect <agatevureglory@gmail.com>
2020-01-25 15:56:23 +0100

    Added DashboardViemodel and Dashboard repository to the module

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/repositoryModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/repositoryModule.kt
@@ -6,6 +6,8 @@ import org.wikiedufoundation.wikiedudashboard.ui.campaign.dao.ActiveCampaignDao
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.repository.ActiveCampaignRepository
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.dao.CourseListDao
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.repository.CourseListRepository
+import org.wikiedufoundation.wikiedudashboard.ui.dashboard.repository.DashboardRepository
+import org.wikiedufoundation.wikiedudashboard.ui.dashboard.repository.DashboardRepositoryImpl
 
 /**
  * Use the [repositoryModule] to creating repository instance
@@ -25,4 +27,5 @@ val repositoryModule = module {
 
     single { provideCampaignListRepository(get(), get()) }
     single { provideCourseListRepository(get(), get()) }
+    single <DashboardRepository>{ DashboardRepositoryImpl(get(), get()) }
 }
--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -4,6 +4,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.dashboard.viewmodel.DashboardViewModel
 
 /**
  * Use the [viewModelModule] to creating viewModel instance
@@ -11,4 +12,5 @@ import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseList
 val viewModelModule = module {
     viewModel { ActiveCampaignViewModel(get()) }
     viewModel { CourseListViewModel(get()) }
+    viewModel  { (cookies :String) -> DashboardViewModel(get(), cookies) }
 }
