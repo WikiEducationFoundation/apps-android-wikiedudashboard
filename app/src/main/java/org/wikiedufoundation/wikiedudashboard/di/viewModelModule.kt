1fe65ca3c451bc0ef4c51eadc9c21abe18c40efe
Glory Agatevure <agatevureglory@gmail.com>
2020-01-26 10:51:56 +0100

    DI modules seperation (#329)
    * Seperated the modules
    
    * Added DashboardPresenter and DashboardProvider
    
    * Lint check
    
    * Added DashboardViemodel and Dashboard repository to the module
    
    * modified respositoryModule and viewmodelModule

---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -0,0 +1,16 @@
+package org.wikiedufoundation.wikiedudashboard.di
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.dashboard.viewmodel.DashboardViewModel
+
+/**
+ * Use the [viewModelModule] to creating viewModel instance
+ **/
+val viewModelModule = module {
+    viewModel { ActiveCampaignViewModel(get()) }
+    viewModel { CourseListViewModel(get()) }
+    viewModel { (cookies: String) -> DashboardViewModel(get(), cookies) }
+}
