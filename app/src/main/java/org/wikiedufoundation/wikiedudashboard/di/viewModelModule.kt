c8f5605092de2f027e22bdc11d237a1873f9ad9c
gconnect <agatevureglory@gmail.com>
2020-01-27 14:00:40 +0100

    Added dsl for recentActivityViewModel to the module

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -3,6 +3,7 @@ package org.wikiedufoundation.wikiedudashboard.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.viewmodel.RecentActivityViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.dashboard.viewmodel.DashboardViewModel
 
@@ -13,4 +14,5 @@ val viewModelModule = module {
     viewModel { ActiveCampaignViewModel(get()) }
     viewModel { CourseListViewModel(get()) }
     viewModel { (cookies: String) -> DashboardViewModel(get(), cookies) }
+    viewModel { (url: String) -> RecentActivityViewModel(get(), url) }
 }
