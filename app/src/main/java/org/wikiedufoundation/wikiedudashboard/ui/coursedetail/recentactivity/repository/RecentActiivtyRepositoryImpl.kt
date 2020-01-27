b3afcd2a4ee5ca1fdd7c08fb36ace584a4002237
gconnect <agatevureglory@gmail.com>
2020-01-27 14:07:13 +0100

    Created recent activity viewmodel

---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/recentactivity/repository/RecentActiivtyRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.repository
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.wikiedufoundation.wikiedudashboard.data.network.WikiEduDashboardApi
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.dao.RecentActivityDao
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.data.RecentActivity
+import org.wikiedufoundation.wikiedudashboard.util.Urls.SUB_URL_COURSE_RECENT
+
+/**Declares the DAO as a private property in the constructor. Pass in the DAO
+ *instead of the whole database, because you only need access to the DAO*
+ * */
+class RecentActiivtyRepositoryImpl(private val wikiEduDashboardApi: WikiEduDashboardApi,
+                                   private val recentActivityDao: RecentActivityDao) : RecentActivityRepository {
+
+    /**
+     * Room executes all queries on a separate thread.he suspend modifier tells the compiler that
+     * this must be called from a coroutine or another suspend function.
+     * */
+    override suspend fun getAllRecentActivities(): List<RecentActivity> = withContext(Dispatchers.IO){
+        recentActivityDao.getAllRecentActiivty()
+    }
+
+    /** The suspend modifier tells the compiler that this must be called from a
+     *  coroutine or another suspend function.
+     **/
+    override suspend fun insertRecentActivity(url: String) = withContext(Dispatchers.IO) {
+        val request = wikiEduDashboardApi.getRecentActivity(SUB_URL_COURSE_RECENT.format(url))
+        val recentList = request.course.revisions
+        recentActivityDao.insertRecentActivity(recentList)
+    }
+}
---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/recentactivity/repository/RecentActivityRepository.kt
@@ -0,0 +1,19 @@
+package org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.repository
+
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.data.RecentActivity
+
+/**
+ *Declares RecentActivityRepository interface
+ * */
+interface RecentActivityRepository {
+
+    /**
+     *Creates a suspend function [getAllRecentActivities]
+     * */
+    suspend fun getAllRecentActivities() : List<RecentActivity>
+
+    /**
+     *Creates a suspend function [insertRecentActivity]
+     * */
+    suspend fun insertRecentActivity(url:String)
+}
