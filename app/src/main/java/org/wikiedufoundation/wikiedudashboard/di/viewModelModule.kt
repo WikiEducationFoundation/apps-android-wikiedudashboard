6501881b1cc044174b146b599bbebcb1c1700514
gconnect <agatevureglory@gmail.com>
2020-01-28 16:11:04 +0100

    Added studentViemodel to the viewmodel dsl

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -3,6 +3,7 @@ package org.wikiedufoundation.wikiedudashboard.di
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.students.viewmodel.StudentsViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.dashboard.viewmodel.DashboardViewModel
 
@@ -13,4 +14,5 @@ val viewModelModule = module {
     viewModel { ActiveCampaignViewModel(get()) }
     viewModel { CourseListViewModel(get()) }
     viewModel { (cookies: String) -> DashboardViewModel(get(), cookies) }
+    viewModel { (cookies: String) -> StudentsViewModel(get(), cookies) }
 }
