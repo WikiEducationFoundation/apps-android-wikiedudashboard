095e2e719af634d71d2e053bcc15b1c7b4712a0b
gconnect <agatevureglory@gmail.com>
2020-01-28 16:16:49 +0100

    Remove safe check

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/util/ShowMessage.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/util/ShowMessage.kt
@@ -4,4 +4,4 @@ package org.wikiedufoundation.wikiedudashboard.util
  * This class is called to display messages
  *
  **/
-class ShowMessage(val showMsg: String?)
+class ShowMessage(val showMsg: String)
