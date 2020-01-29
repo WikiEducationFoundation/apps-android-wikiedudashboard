ce842729e430d201f67acd04f3fb1afa956e5d80
gconnect <agatevureglory@gmail.com>
2020-01-29 15:26:50 +0100

    Added courseDetail to viewModelModule

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -4,6 +4,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.articlesedited.viewmodel.ArticlesEditedViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.viewmodel.CourseDetailViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.students.viewmodel.StudentsViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.viewmodel.RecentActivityViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
@@ -19,4 +20,5 @@ val viewModelModule = module {
     viewModel { (cookies: String) -> StudentsViewModel(get(), cookies) }
     viewModel { (url: String) -> RecentActivityViewModel(get(), url) }
     viewModel { (url: String) -> ArticlesEditedViewModel(get(), url) }
+    viewModel {CourseDetailViewModel(get()) }
 }
