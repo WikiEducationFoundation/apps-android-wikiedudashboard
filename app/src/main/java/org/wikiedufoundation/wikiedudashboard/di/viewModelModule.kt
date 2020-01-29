30db10d497173bcf01b82c86720c545e69afce46
gconnect <agatevureglory@gmail.com>
2020-01-29 12:57:47 +0100

    Added CourseUploads to the viewModelModule

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/viewModelModule.kt
@@ -5,6 +5,7 @@ import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.viewmodel.ActiveCampaignViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.students.viewmodel.StudentsViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.recentactivity.viewmodel.RecentActivityViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.uploads.viewmodel.CourseUploadsViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.courselist.viewmodel.CourseListViewModel
 import org.wikiedufoundation.wikiedudashboard.ui.dashboard.viewmodel.DashboardViewModel
 
@@ -17,4 +18,5 @@ val viewModelModule = module {
     viewModel { (cookies: String) -> DashboardViewModel(get(), cookies) }
     viewModel { (cookies: String) -> StudentsViewModel(get(), cookies) }
     viewModel { (url: String) -> RecentActivityViewModel(get(), url) }
+    viewModel { (url: String) -> CourseUploadsViewModel(get(), url) }
 }
