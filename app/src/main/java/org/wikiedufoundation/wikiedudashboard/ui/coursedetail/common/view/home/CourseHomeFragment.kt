ac9b1d8e855e7292e4181d259b3c918d1de19781
gconnect <agatevureglory@gmail.com>
2020-01-29 15:27:59 +0100

    Modified course home fragment

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/common/view/home/CourseHomeFragment.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/coursedetail/common/view/home/CourseHomeFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_course_home.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.wikiedufoundation.wikiedudashboard.R
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.data.CourseDetail
+import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.viewmodel.CourseDetailViewModel
+import org.wikiedufoundation.wikiedudashboard.ui.courselist.data.CourseListData
 import java.text.MessageFormat
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -36,6 +40,10 @@ class CourseHomeFragment : Fragment() {
     ): View? = inflater.inflate(R.layout.fragment_course_home, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setData()
+    }
+
+    private fun setData(){
         textViewCountArticlesCreated.text = courseDetail?.createdCount
         textViewCountArticlesEdited.text = courseDetail?.editedCount
         textViewCountTotalEdits.text = courseDetail?.editCount
@@ -49,7 +57,6 @@ class CourseHomeFragment : Fragment() {
         textViewCourseTermDetail.text = courseDetail?.term
         textViewCoursePassCodeDetail.text = courseDetail?.passCode
         textViewCourseExpectedStudentsDetail.text = MessageFormat.format("{0}", courseDetail?.expectedStudents)
-
         textViewCourseStartDetail.text = readableDate(courseDetail?.start)
         textViewCourseEndDetail.text = readableDate(courseDetail?.end)
     }
@@ -57,14 +64,12 @@ class CourseHomeFragment : Fragment() {
     companion object {
         // TODO: Rename parameter arguments, choose names that match
         // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
+        private  const val  ARG_PARAM1 = "param1"
 
-        fun newInstance(courseDetail: CourseDetail): CourseHomeFragment {
-            val fragment = CourseHomeFragment()
+        fun newInstance(courseDetail: CourseDetail) = CourseHomeFragment().apply {
             val args = Bundle()
             args.putSerializable(ARG_PARAM1, courseDetail)
-            fragment.arguments = args
-            return fragment
+            this.arguments = args
         }
 
         /**
