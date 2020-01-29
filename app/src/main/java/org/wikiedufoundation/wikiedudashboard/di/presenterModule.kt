1c3dcce7101cf94f72d88a2f942c95278dc68d7a
gconnect <agatevureglory@gmail.com>
2020-01-29 15:24:32 +0100

    Removed courseDetail from presenterModule

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/presenterModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/presenterModule.kt
@@ -1,10 +1,6 @@
 package org.wikiedufoundation.wikiedudashboard.di
 
 import org.koin.dsl.module
-import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.presenter.CourseDetailPresenter
-import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.presenter.CourseDetailPresenterImpl
-import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.provider.CourseDetailProvider
-import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.view.CourseDetailView
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.uploads.presenter.CourseUploadsPresenter
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.uploads.presenter.CourseUploadsPresenterImpl
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.uploads.provider.CourseUploadsProvider
@@ -56,24 +52,6 @@ val presenterModule = module {
         ProfilePresenterImpl(view, provider)
     }
 
-    /**
-     * Factory for [CourseDetailPresenter] injecting the [CourseDetailView] and [CourseDetailProvider]
-     **/
-
-    /**
-     * Factory for [CourseDetailPresenter] injecting the [CourseDetailView] and [CourseDetailProvider]
-     **/
-
-    /**
-     * Factory for [CourseDetailPresenter] injecting the [CourseDetailView] and [CourseDetailProvider]
-     **/
-
-    /**
-     * Factory for [CourseDetailPresenter] injecting the [CourseDetailView] and [CourseDetailProvider]
-     **/
-    factory<CourseDetailPresenter> { (view: CourseDetailView, provider: CourseDetailProvider) ->
-        CourseDetailPresenterImpl(view, provider)
-    }
 
     /**
      * Factory for [CourseUploadsPresenter] injecting the [CourseUploadsView] and [CourseUploadsProvider]
