d49f049c7f6911977a244e38d7a8ba99f936645b
gconnect <agatevureglory@gmail.com>
2020-01-31 10:07:11 +0100

    Removed ProfilePresenter from the presenterModule

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/presenterModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/presenterModule.kt
@@ -3,8 +3,6 @@ package org.wikiedufoundation.wikiedudashboard.di
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.mediadetail.MediaDetailsContract
 import org.wikiedufoundation.wikiedudashboard.ui.mediadetail.MediaDetailsPresenterImpl
-import org.wikiedufoundation.wikiedudashboard.ui.profile.ProfileContract
-import org.wikiedufoundation.wikiedudashboard.ui.profile.ProfilePresenterImpl
 
 /**
  * Use the [presenterModule] to creating the mvp presenter for each view
@@ -29,22 +27,5 @@ val presenterModule = module {
         MediaDetailsPresenterImpl(view, provider)
     }
 
-    /**
-     * Factory for [ProfileContract.Presenter] injecting the [ProfileContract.View] and [ProfileContract.Provider]
-     **/
 
-    /**
-     * Factory for [ProfileContract.Presenter] injecting the [ProfileContract.View] and [ProfileContract.Provider]
-     **/
-
-    /**
-     * Factory for [ProfileContract.Presenter] injecting the [ProfileContract.View] and [ProfileContract.Provider]
-     **/
-
-    /**
-     * Factory for [ProfileContract.Presenter] injecting the [ProfileContract.View] and [ProfileContract.Provider]
-     **/
-    factory<ProfileContract.Presenter> { (view: ProfileContract.View, provider: ProfileContract.Provider) ->
-        ProfilePresenterImpl(view, provider)
-    }
 }
