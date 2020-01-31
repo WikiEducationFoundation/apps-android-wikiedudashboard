ad49fb997c33bd4bbc73b50c5bf16f760a433cd9
gconnect <agatevureglory@gmail.com>
2020-01-31 17:34:29 +0100

    Modified courseListRepository

---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/courselist/repository/CourseListRepository.kt
@@ -0,0 +1,20 @@
+package org.wikiedufoundation.wikiedudashboard.ui.courselist.repository
+
+import androidx.lifecycle.LiveData
+import org.wikiedufoundation.wikiedudashboard.ui.courselist.data.CourseListData
+
+/**
+ * Declares the CourseListRepository interface
+ * */
+interface CourseListRepository {
+
+    /**
+    * This function gets all the courses
+    **/
+    fun allCourseList(): LiveData<List<CourseListData>>
+
+    /** The suspend modifier tells the compiler that this must be called from a
+     *  coroutine or another suspend function.
+     **/
+    suspend fun requestCourseList(cookies: String)
+}
