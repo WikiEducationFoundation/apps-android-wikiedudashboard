99339241b7c97849bf26324fb61f9521030902ee
gconnect <agatevureglory@gmail.com>
2020-01-31 14:58:46 +0100

    Removed mediaDetails from the presenterModule

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/presenterModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/presenterModule.kt
@@ -1,8 +1,6 @@
 package org.wikiedufoundation.wikiedudashboard.di
 
 import org.koin.dsl.module
-import org.wikiedufoundation.wikiedudashboard.ui.mediadetail.MediaDetailsContract
-import org.wikiedufoundation.wikiedudashboard.ui.mediadetail.MediaDetailsPresenterImpl
 import org.wikiedufoundation.wikiedudashboard.ui.profile.ProfileContract
 import org.wikiedufoundation.wikiedudashboard.ui.profile.ProfilePresenterImpl
 
@@ -10,24 +8,6 @@ import org.wikiedufoundation.wikiedudashboard.ui.profile.ProfilePresenterImpl
  * Use the [presenterModule] to creating the mvp presenter for each view
  **/
 val presenterModule = module {
-    /**
-     * Factory for [MediaDetailsContract.Presenter] injecting the [MediaDetailsContract.View] and [MediaDetailsContract.Provider]
-     **/
-
-    /**
-     * Factory for [MediaDetailsContract.Presenter] injecting the [MediaDetailsContract.View] and [MediaDetailsContract.Provider]
-     **/
-
-    /**
-     * Factory for [MediaDetailsContract.Presenter] injecting the [MediaDetailsContract.View] and [MediaDetailsContract.Provider]
-     **/
-
-    /**
-     * Factory for [MediaDetailsContract.Presenter] injecting the [MediaDetailsContract.View] and [MediaDetailsContract.Provider]
-     **/
-    factory<MediaDetailsContract.Presenter> { (view: MediaDetailsContract.View, provider: MediaDetailsContract.Provider) ->
-        MediaDetailsPresenterImpl(view, provider)
-    }
 
     /**
      * Factory for [ProfileContract.Presenter] injecting the [ProfileContract.View] and [ProfileContract.Provider]
