17f390ff712a2674fcebf946701650bc0854c89d
gconnect <agatevureglory@gmail.com>
2020-02-07 11:31:45 +0100

    Created Article Fragment

---
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/campaigndetails/article/view/ArticleFragment.kt
@@ -0,0 +1,34 @@
+package org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.article.view
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.wikiedufoundation.wikiedudashboard.R
+
+import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.article.viewmodel.ArticleViewModel
+
+class ArticleFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ArticleFragment()
+    }
+
+    private lateinit var viewModel: ArticleViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.article_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(ArticleViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
