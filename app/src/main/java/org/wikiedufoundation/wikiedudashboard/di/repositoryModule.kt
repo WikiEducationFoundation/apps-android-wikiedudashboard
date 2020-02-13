179e10b8e869273219c9e78648111ebbe94c6a9f
gconnect <agatevureglory@gmail.com>
2020-02-13 18:14:50 +0100

    Added course repository to the repository module

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/repositoryModule.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/di/repositoryModule.kt
@@ -3,6 +3,8 @@ package org.wikiedufoundation.wikiedudashboard.di
 import org.koin.dsl.module
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.repository.ActiveCampaignRepository
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.repository.ActiveCampaignRepositoryImpl
+import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.course.repsoitory.CourseRepository
+import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.course.repsoitory.CourseRepositoryImpl
 import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.home.repsoitory.CampaignDetailRepository
 import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.home.repsoitory.CampaignDetailRepositoryImpl
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.articlesedited.repository.ArticlesEditedRepository
@@ -39,4 +41,5 @@ val repositoryModule = module {
     single <ProfileRepository> { ProfileRepositoryImpl(get()) }
     single <MediaDetailsRepository> { MediaDetailsRepositoryImpl(get()) }
     single <CampaignDetailRepository> { CampaignDetailRepositoryImpl(get()) }
+    single <CourseRepository> { CourseRepositoryImpl(get()) }
 }
