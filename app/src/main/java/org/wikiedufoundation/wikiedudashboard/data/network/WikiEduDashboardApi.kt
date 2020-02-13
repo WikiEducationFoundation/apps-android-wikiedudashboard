ec58fe851da9830e9c9a19df2957b855d53383a9
gconnect <agatevureglory@gmail.com>
2020-02-13 18:14:16 +0100

    Added Course API

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/data/network/WikiEduDashboardApi.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/data/network/WikiEduDashboardApi.kt
@@ -1,6 +1,7 @@
 package org.wikiedufoundation.wikiedudashboard.data.network
 
 import org.wikiedufoundation.wikiedudashboard.ui.campaign.data.ExploreCampaignsResponse
+import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.course.data.CourseResponse
 import org.wikiedufoundation.wikiedudashboard.ui.campaigndetails.home.data.CampaignDetailsResponse
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.articlesedited.data.ArticlesEdited
 import org.wikiedufoundation.wikiedudashboard.ui.coursedetail.common.data.CourseDetailResponse
@@ -29,6 +30,12 @@ interface WikiEduDashboardApi {
      ***/
     @GET
     suspend fun getCampaignDetail(@Url url: String): CampaignDetailsResponse
+
+    /**
+     * This API is used to fetch list of course of a campaign.
+     ***/
+    @GET
+    suspend fun getCourse(@Url url: String): CourseResponse
 
     /**
      * This API is used to fetch list of articles of a course.
