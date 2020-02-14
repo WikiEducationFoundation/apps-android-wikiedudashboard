de07714d7c0b29e745cc536d5ed95bc85cec1c48
gconnect <agatevureglory@gmail.com>
2020-02-14 10:25:56 +0100

    Modified Course detailed fragment

--- app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/campaigndetails/course/view/CourseDetailFragment.kt
+++ app/src/main/java/org/wikiedufoundation/wikiedudashboard/ui/campaigndetails/course/view/CourseDetailFragment.kt
@@ -19,9 +19,9 @@ class CourseDetailFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentCourseDetailBinding
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_course_detail,
